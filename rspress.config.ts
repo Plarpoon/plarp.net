c13bab6d14fcacbb512735b6dc475ed5a0dcaec2
Alessandro Suha <github@plarp.net>
2025-03-23 22:44:32 +0100

    feat: cleanup home

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,18 +3,18 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  title: 'My Site',
-  icon: '/rspress-icon.png',
+  title: 'plarp.net',
+  icon: '/lw.png',
   logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
+    light: '/SisselCat.webp',
+    dark: '/SisselCat.webp',
   },
   themeConfig: {
     socialLinks: [
       {
         icon: 'github',
         mode: 'link',
-        content: 'https://github.com/web-infra-dev/rspress',
+        content: 'https://github.com/plarpoon/plarp.net',
       },
     ],
   },
